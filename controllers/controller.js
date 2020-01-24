7140ca5d29992d1a29c4f4a387517d34844d98d6
naoyama88 <naoyama88@gmail.com>
2020-01-24 15:01:39 -0800

    changed the way to get background image

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,5 @@
 const Riddle = require("../models/riddle");
 const Comment = require("../models/comment");
-const fs = require("fs");
 const util = require("../util/util");
 
 exports.getHomePage = (req, res, next) => {
@@ -17,30 +16,19 @@ exports.createRiddle = (req, res, next) => {
 };
 
 exports.postRiddle = (req, res, next) => {
-  const riddle = new Riddle(req.body.author, req.body.title, req.body.content);
+  const riddle = new Riddle(req.body.author, req.body.title, req.body.content, util.getRandomBgImg());
   riddle.saveRiddle();
   res.redirect("/");
 };
 
 exports.detailRiddle = (req, res, next) => {
   // prepare background image
-  const bgiImgFolder = __dirname + "/../public/img/riddle_background";
-  const imgFiles = fs.readdirSync(bgiImgFolder);
-  let bgImgFile = "/img/riddle_background/default.png";
-  if (imgFiles && imgFiles.length !== 1) {
-    bgImgFile =
-      "/img/riddle_background/" +
-      imgFiles[Math.floor(Math.random() * Math.floor(imgFiles.length))];
-  }
-  if (req.query.imgUrl) {
-    bgImgFile = req.query.imgUrl;
-  }
-
   const riddleId = req.params.riddleId;
   const isEditing = req.query.edit;
 
   Riddle.getOne(riddleId).then(riddle => {
     riddle.date = util.getFormattedDate(riddle.date);
+    const bgImgFile = riddle.image_url || util.getRandomBgImg();
     Comment.getAllComment().then(comments => {
       res.render("detail", {
         riddle,
