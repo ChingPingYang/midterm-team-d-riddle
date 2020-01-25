f0e0d31ca686ed10017c60fc43b503c3ce3bd244
Tatsuya167 <clark.taka16@gmail.com>
2020-01-25 10:24:18 +0900

    modified some scss and add script to prevent duplicate sbmission

---
+++ public/script/preventDuplicateSubmission.js
@@ -0,0 +1,5 @@
+function pvDbForm() {
+    // document.getElementsByTagName("INPUT")[type="submit"].setAttribute("disabled");
+    document.getElementById("btnSubmit").setAttribute("disabled", "disabled")
+    document.getElementById("submission").submit();
+}
