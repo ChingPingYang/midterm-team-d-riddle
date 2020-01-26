e914c84695e27abb59798bccb41de960824529db
naoyama88 <naoyama88@gmail.com>
2020-01-25 19:15:16 -0800

    format

--- models/riddle.js
+++ models/riddle.js
@@ -2,46 +2,42 @@ const database = require('../util/database');
 const mongodb = require('mongodb');
 
 class Riddle {
-    constructor(author, title, riddle, image_url, date) {
-        this.title = title;
-        this.author = author;
-        this.riddle = riddle;
-        this.like = 0;
-        this.image_url = image_url;
-        this.date = new Date();
-    }
+  constructor(author, title, riddle, image_url, date) {
+    this.title = title;
+    this.author = author;
+    this.riddle = riddle;
+    this.like = 0;
+    this.image_url = image_url;
+    this.date = new Date();
+  }
 
-    saveRiddle() {
-        database.getDB().collection('riddles').insertOne(this)
-            
-    }
+  saveRiddle() {
+    database.getDB().collection('riddles').insertOne(this);
+  }
 
-    static getAll() {
-        return database.getDB().collection('riddles').find().toArray()
-            
-    }
+  static getAll() {
+    return database.getDB().collection('riddles').find().toArray()
+  }
 
-    static getOne(riddleId) {
-        return database.getDB().collection('riddles').findOne({ _id: new mongodb.ObjectId(riddleId) })
-            
-    }
+  static getOne(riddleId) {
+    return database.getDB().collection('riddles').findOne({ _id: new mongodb.ObjectId(riddleId) })
+  }
 
-    static deleteRiddle(id) {
-        return database.getDB().collection('riddles').deleteOne({_id: new mongodb.ObjectId(id)});
-        // TODO delete all comments related to this riddle
-    }
+  static deleteRiddle(id) {
+    return database.getDB().collection('riddles').deleteOne({_id: new mongodb.ObjectId(id)});
+  }
 
-    static updateRiddle(id, title, content) {
-        return database.getDB().collection('riddles')
-            .updateOne({_id: new mongodb.ObjectId(id)}, {$set: {title: title, content: content}});
-    }
+  static updateRiddle(id, title, content) {
+    return database.getDB().collection('riddles')
+      .updateOne({_id: new mongodb.ObjectId(id)}, {$set: {title: title, content: content}});
+  }
 
-    static async like(id) {
-        const collection = database.getDB().collection('riddles');
-        const whereClause = { _id: new mongodb.ObjectId(id) };
-        const riddle = await collection.findOne(whereClause);
-        return await collection.updateOne(whereClause, { $set: { like: ++riddle.like } });
-    }
+  static async like(id) {
+    const collection = database.getDB().collection('riddles');
+    const whereClause = { _id: new mongodb.ObjectId(id) };
+    const riddle = await collection.findOne(whereClause);
+    return await collection.updateOne(whereClause, { $set: { like: ++riddle.like } });
+  }
 }
 
 module.exports = Riddle;
