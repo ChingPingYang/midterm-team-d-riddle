150bb65b2f64fa1813cbf241e82e45f28b5c8d8c
JamesYang <gn00999829@gmail.com>
2020-02-13 14:21:56 -0800

    Dploy

--- util/database.js
+++ util/database.js
@@ -4,7 +4,8 @@ let db;
 
 exports.mongoConnect = callback => {
   MongoClient.connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_NAME}/riddle?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_NAME}/riddle?retryWrites=true&w=majority`,
+    {useUnifiedTopology: true}
   )
     .then(client => {
       db = client.db("riddle");
